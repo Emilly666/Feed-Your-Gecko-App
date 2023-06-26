598a1bc6c42e186a13f7670142135ea601a0f7a9
Emilia <kalanskae@gmail.com>
2023-06-26 18:20:53 +0200

    icons cleanup

--- app/src/main/java/com/example/feed_your_gecko_app/tasks/HomeViewAdapter.java
+++ app/src/main/java/com/example/feed_your_gecko_app/tasks/HomeViewAdapter.java
@@ -70,9 +70,9 @@ public class HomeViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
         int icon;
         if(mItemList.get(position).getType() == VIEW_TYPE_ITEM_FEED){
-            icon = R.drawable.watering_can_24;
+            icon = R.drawable.baseline_add_24;
         }else{
-            icon = R.drawable.fertilizer_24;
+            icon = R.drawable.baseline_add_24;
         }
 
         viewHolder.button4.setCompoundDrawablesWithIntrinsicBounds(icon, 0, R.drawable.baseline_arrow_drop_down_24, 0);
